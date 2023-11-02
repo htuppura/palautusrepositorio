71a691929b483a5ff5cc85a809c428ed6953933a
Maj-Lis Pääkkönen <mpaakkon@local>
2023-11-03 00:10:01 +0200

    Viikko 2: 10. Pelaajalista

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,15 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.team = dict['team']
+        self.nationality = dict['nationality']
     
     def __str__(self):
-        return self.name
+        n = self.name
+        t = self.team
+        g = self.goals
+        a = self.assists
+        p = self.goals + self.assists
+        return f"{n} team {t} - {g} + {a} = {p}"
